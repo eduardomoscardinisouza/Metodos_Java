55f056d647aec54cfd344a5bc7ec3e83e06f10a7
eduardomoscardinisouza <eduardo.fca@gmail.com>
2022-03-29 21:58:23 -0300

    Commit Exercicio 2

---
+++ src/Metodos_Java_Exercicio2/Main.java
@@ -0,0 +1,14 @@
+package Metodos_Java_Exercicio2;
+
+public class Main {
+
+    public static void main(String [] args) {
+
+        // Quadrilatero
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 8);
+        Quadrilatero.area(7, 8, 9);
+
+    }
+}
